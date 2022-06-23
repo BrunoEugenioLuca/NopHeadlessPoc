77a1c8ed911dcd4ced9a4ff42bd95931d485939a
Eugenio Luca Bruno <brunoeugeniol95@outlook.it>
2022-06-23 09:27:23 +0200

    cretae new plugin Nop.Plugin.Misc.NopHeadessController

---
+++ src/Plugins/Nop.Plugin.Misc.NopHeadlessController/NopHeadlessController.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Nop.Services.Plugins;
+
+namespace Nop.Plugin.Misc.NopHeadlessController
+{
+    public class NopHeadlessController : BasePlugin
+    {
+
+        public override Task InstallAsync()
+        {
+            return base.InstallAsync();
+        }
+
+        public override Task UninstallAsync()
+        {
+            return base.UninstallAsync();
+        }
+    }
+}
